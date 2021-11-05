5afc8906ee593a32225d4b29c5f2326b1b99d3d5
Derek Vogel <78238679+Dvogel345@users.noreply.github.com>
2021-11-05 15:54:39 -0400

    added class Mercury etending to Vehicle with sonstructor and super params

---
+++ mercurySedan.js
@@ -0,0 +1,19 @@
+//this includes the vehicle class as a module
+const VehicleModule = require("./vehicleBaseClass")
+
+//this shows how to call from this module...
+let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
+console.log(v.make)
+
+class Mercury extends v{
+    constructor(make, model, year, color, mileage){
+        super(make, model, year, color, mileage);
+
+        this.maximumPassangers = 6;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maximumSpeed = 200;
+        this.fuel = 100;
+        this.scheduleService = false;
+    } 
+}
